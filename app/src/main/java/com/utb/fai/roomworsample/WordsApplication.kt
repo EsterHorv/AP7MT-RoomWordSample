b06c88ab9068521b3685a3d4d554ddd508d8242a
student <student@example.com>
2021-11-24 17:36:00 +0100

    RoomDatabase edited

--- app/src/main/java/com/utb/fai/roomworsample/WordsApplication.kt
+++ app/src/main/java/com/utb/fai/roomworsample/WordsApplication.kt
@@ -3,10 +3,17 @@ package com.utb.fai.roomworsample
 import android.app.Application
 import com.utb.fai.roomworsample.database.WordRepository
 import com.utb.fai.roomworsample.database.WordRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication : Application() {
-    // Using by lazy so the database and the repository are only created when they're needed
-    // rather than when the application starts
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
-    val repository by lazy { WordRepository(database.wordDao()) }
+    class WordsApplication : Application() {
+        // No need to cancel this scope as it'll be torn down with the process
+        val applicationScope = CoroutineScope(SupervisorJob())
+
+        // Using by lazy so the database and the repository are only created when they're needed
+        // rather than when the application starts
+        val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
+        val repository by lazy { WordRepository(database.wordDao()) }
+    }
 }
