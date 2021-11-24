9fff909c0ebff75f201f543fb61d79431bbce193
student <student@example.com>
2021-11-24 17:41:57 +0100

    edited: colors.xml, strings.xml, dimens.xml

---
+++ app/src/main/java/com/utb/fai/roomworsample/database/NewWordActivity.kt
@@ -0,0 +1,12 @@
+package com.utb.fai.roomworsample.database
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.utb.fai.roomworsample.R
+
+class NewWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
