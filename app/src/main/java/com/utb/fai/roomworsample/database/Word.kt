a20bdb3737d79726960ac45edddb64b5e1cb05d5
student <student@example.com>
2021-11-24 16:45:47 +0100

    word entity class edit

---
+++ app/src/main/java/com/utb/fai/roomworsample/database/Word.kt
@@ -0,0 +1,8 @@
+package com.utb.fai.roomworsample.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table") /* vytvoření entity*/
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
